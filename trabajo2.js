148fc390ac36264b88fcce0f32d685b3bd3903b7
Douma242 <danieldouma@hotmail.es>
2017-08-07 14:19:18 -0600

    :D
    :b:

---
+++ trabajo2.js
@@ -0,0 +1,17 @@
+var edad=0;
+switch(edad){
+case 0:
+console.log("sos bebé");
+break;
+case 1:
+console.log("sos niño");
+break;
+case 17:
+console.log("adolescente");
+break;
+case 30:
+console.log("adulto");
+break;
+default:
+console.log("no se que etiqueta ponerte");
+}
---
+++ trabajo3.js
@@ -0,0 +1,43 @@
+var personas = [
+{
+firstName: 'Ricardo',
+lastName: 'Juarez',
+address:{
+country:{countryName: 'Guatemala', areacode:'502'},
+city:'Guatemala',
+}
+age:'28',
+height:'180cm'
+},
+{
+firstName: 'Erick',
+lastName: 'Castillo',
+address:{
+country:{countryName: 'adios', areacode:'687'},
+city:'hola',
+}
+age:'33',
+height:'156cm'
+},
+{
+firstName: 'Willi',
+lastName: 'Ortega',
+address:{
+country:{countr0yName: 'USA', areacode:'Wachinton'},
+city:'Wachinton',
+}
+age:'55',
+height:'145cm'
+},
+{
+firstName: 'Maria',
+lastName: 'Auxiliadora',
+address:{
+country:{countryName: 'España', areacode:'bebe'},
+city:'guau',
+}
+age:'44',
+height:'150cm'
+},
+
+]
